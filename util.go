d4c04c1664bbcb7560e8d60e26de519b023639de
Muninn <hyacinthus@gmail.com>
2018-12-15 11:15:45 +0800

    add tag list

--- util.go
+++ util.go
@@ -54,3 +54,46 @@ func (f *FileURL) Scan(src interface{}) error {
 func (f FileURL) Value() (driver.Value, error) {
 	return string(f), nil
 }
+
+// Tags ===========标签列表===========
+type Tags []string
+
+// String 转换为string类型
+func (t Tags) String() string {
+	var s = []string(t)
+	return strings.Join(s, ",")
+}
+
+// MarshalJSON 转换为json类型 加域名
+func (t Tags) MarshalJSON() ([]byte, error) {
+	return json.Marshal([]string(t))
+}
+
+// UnmarshalJSON 不做处理
+func (t *Tags) UnmarshalJSON(data []byte) error {
+	var tmp []string
+	if err := json.Unmarshal(data, &tmp); err != nil {
+		return err
+	}
+	*t = Tags(tmp)
+	return nil
+}
+
+// Scan implements the Scanner interface.
+func (t *Tags) Scan(src interface{}) error {
+	if src == nil {
+		*t = make([]string, 0)
+		return nil
+	}
+	tmp, ok := src.([]byte)
+	if !ok {
+		return errors.New("Read tags from DB failed")
+	}
+	*t = strings.Split(string(tmp), ",")
+	return nil
+}
+
+// Value implements the driver Valuer interface.
+func (t Tags) Value() (driver.Value, error) {
+	return t.String(), nil
+}
